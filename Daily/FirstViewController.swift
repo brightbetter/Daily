4c4b74063e85ea0a8eecf16bb358ab682d3db93b
bright <bright.wu@dingtone.me>
2020-03-17 14:27:35 +0800

    Initial Commit

---
+++ Daily/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Daily
+//
+//  Created by Bright on 2020/3/17.
+//  Copyright © 2020 Bright. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
